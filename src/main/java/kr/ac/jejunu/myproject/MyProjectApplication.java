7828aee9ec36f0ad89e9bf1f612d4a2c80ccab36
dhrgodms <dhrgodms123@gmail.com>
2023-10-31 03:05:21 +0900

    modify : build.gradle:

--- src/main/java/kr/ac/jejunu/myproject/MyProjectApplication.java
+++ src/main/java/kr/ac/jejunu/myproject/MyProjectApplication.java
@@ -2,16 +2,23 @@ package kr.ac.jejunu.myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @EnableJpaAuditing // auditing 기능 활성화
-public class MyProjectApplication {
+public class MyProjectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MyProjectApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return super.configure(builder);
     }
 
     @Bean
